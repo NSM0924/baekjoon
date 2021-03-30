fd04c3910e912e4569b5289e193b618d5501d494
NSM0924 <nasungmin50@naver.com>
2021-03-30 22:24:33 +0900

    .

---
+++ _2869.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class _2869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int V = sc.nextInt();
+        sc.close();
+
+        int result = (V-B)/(A-B);
+        if ((V-B)%(A-B)!=0){
+            result++;
+        }
+        System.out.println(result);
+    }
+}
